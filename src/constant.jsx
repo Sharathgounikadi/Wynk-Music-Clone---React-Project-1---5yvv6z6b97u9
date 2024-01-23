165595a401b7b4b88e08dee9bc92ea7cc1459bb1
Sharath <sharathgaunikadi12007@gmail.com>
2024-01-23 23:35:30 +0530

    code

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,5 +1,5 @@
 // constant.js
-export const PROJECT_ID = "ofq02vi0zi2i";
+export const PROJECT_ID = "2C5yvv6z6b97u9";
 export const APP_TYPE = "music";
 export const LOGIN_API = "https://academics.newtonschool.co/api/v1/user/login";
 export const SIGNUP_API = "https://academics.newtonschool.co/api/v1/user/signup";
